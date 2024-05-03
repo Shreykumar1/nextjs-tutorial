681f95572b5dc206057009dd7764a20969011d80
Shrey Kumar <shreykumar191@gmail.com>
2024-05-03 17:14:32 +0530

    Next Basics

--- app/about/info/page.js
+++ app/about/info/page.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
 
-const HomePage = () => {
+const InfoPage = () => {
   return (
     <div>
-      <h1 className='text-7xl'>Home Page</h1>
+      <h1 className='text-7xl'>Info Page</h1>
+      <Link href='/' className='text-2xl' >Home Page</Link>
       <Link href='/about' className='text-2xl' >About Page</Link>
-
     </div>
   )
 }
 
-export default HomePage
+export default InfoPage
---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className='text-7xl'>About Page</h1>
+      <Link href='/' className='text-2xl' >Home Page</Link>
+      <Link href='/about/info' className='text-2xl' >Info Page</Link>
+    </div>
+  )
+}
+
+export default AboutPage
